fd35f8bee2d751a7980e84ded2638137118b5d76
Renato Angeline <reangeline@hotmail.com>
2020-04-09 18:51:23 -0300

    Projects Conceito  ReactJS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+  //handleRemoveRepository eu inclui o handle de remover entre [] porem nao estava passando no teste, por que?
+
   async function handleAddRepository() {
-    // TODO
+ 
+    const response = await api.post('/repositories', {
+      url: "https://github.com/reangeline",
+      title: "teste",
+      techs: ["React", "Node"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    document.getElementById(id).remove();
+    await api.delete(`/repositories/${id}`);
+    
   }
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      {repositories.map(repositories => <li id={repositories.id} key={repositories.id}>{repositories.title}<button onClick={() => handleRemoveRepository(repositories.id)}>Remover</button></li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
